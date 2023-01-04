e206fc89264bb9dd2e452d0fdc4de8e8d9771847
Sunday  Jimoh <31360475+sunday4me@users.noreply.github.com>
2023-01-04 11:09:30 +0000

    Adding more

--- main.py
+++ main.py
@@ -4,4 +4,10 @@
 
 Print ("USD to any currency Converter")
 
-Print("Lst of availablecurrencies: \n 1 -> GHS (Ghana Cedis)  \n 2 -> D  \n 3 -> GHS (Ghana Cedis)
+Print("List of availablecurrencies: \n 1 -> NGN (NIgeria Naira Cedis)  \n 2 -> D  \n 3 -> GHS (Ghana Cedis)  4 ->BIRR  5 -> Ksh ")
+
+currency_dict = {"NGN": 700  "D": 62.0 "GHS": 13.9 "BIRR" : 53.2,  "Ksh": 122.65}
+
+currency = "NGN"
+
+curr_num = int(input("Choose your currency's number : "))
