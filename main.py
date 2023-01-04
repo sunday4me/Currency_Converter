fa9684467e0426cf49cb6057d73ba1df11ae5d85
Sunday  Jimoh <31360475+sunday4me@users.noreply.github.com>
2023-01-04 11:00:04 +0000

    adding the main file

---
+++ main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+""" Currency converter (from USD to GHS) """
+
+Print ("USD to any currency Converter")
+
+Print("Lst of availablecurrencies: \n 1 -> GHS (Ghana Cedis)  \n 2 -> D  \n 3 -> GHS (Ghana Cedis)
